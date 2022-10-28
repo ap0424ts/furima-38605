c6ef61c79b7cb6f17dd9ede5ebc9a93ae0773511
ap0424ts <ap0424ts@icloud.com>
2022-10-28 17:45:41 +0900

    バリデーション、エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}+\z/i}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
+  validates :birthday, presence: true
 end
