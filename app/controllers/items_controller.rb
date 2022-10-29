47025fa2573cc2ca70b80ef2a0f63488f1b772f9
ap0424ts <ap0424ts@icloud.com>
2022-10-29 17:25:56 +0900

    ActiveStrageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :category_id, :condition_id, :charge_id, :area_id, :day_id, :image).merge(user_id: current_user.id)
+  end
 end
