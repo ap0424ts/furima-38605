11b1061252c8190e4d7a0a363d9da31c0233d911
ap0424ts <ap0424ts@icloud.com>
2022-10-29 18:04:13 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area, :category, :charge, :condition, :day
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :content, :price, :user_id, presence: true
+  validates :area_id, :category_id, :charge_id, :condition_id, :day_id, numericality: { other_than: 1 } 
 end
