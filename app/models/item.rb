d543af4af5803f6329988633d78b4428b5de2556
ap0424ts <ap0424ts@icloud.com>
2022-10-30 20:25:10 +0900

    Itemモデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,16 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :name, :content, :user_id, :image, presence: true
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :content, presence: true, length: { maximum: 1000 }
   validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
   validates :area_id, :category_id, :charge_id, :condition_id, :day_id, numericality: { other_than: 1 } 
   
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :area, :category, :charge, :condition, :day
+  belongs_to :area
+  belongs_to :category
+  belongs_to :charge
+  belongs_to :condition
+  belongs_to :day
 end
