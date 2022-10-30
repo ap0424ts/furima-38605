25b42939350221f832286e55c3ed927910b790ec
ap0424ts <ap0424ts@icloud.com>
2022-10-30 15:07:44 +0900

    new/createアクションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,11 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+  belongs_to :user
+
+  validates :name, :content, :user_id, :image, presence: true
+  validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
+  validates :area_id, :category_id, :charge_id, :condition_id, :day_id, numericality: { other_than: 1 } 
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area, :category, :charge, :condition, :day
-  belongs_to :user
-  has_one_attached :image
-
-  validates :name, :content, :price, :user_id, presence: true
-  validates :area_id, :category_id, :charge_id, :condition_id, :day_id, numericality: { other_than: 1 } 
 end
--- db/migrate/20221029081114_create_items.rb
+++ db/migrate/20221029081114_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string    :name,         null: false
       t.text      :content,      null: false
       t.integer   :price,        null: false
-      t.eferences :user,         null: false, foreign_key: true 
+      t.references :user,        null: false, foreign_key: true 
       t.string    :category_id,  null: false
       t.integer   :condition_id, null: false
       t.integer   :charge_id,    null: false
