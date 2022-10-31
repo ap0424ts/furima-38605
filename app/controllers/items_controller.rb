b0cbfbc0ca73eab3b9f31cc43451ce96d32deebe
ap0424ts <ap0424ts@icloud.com>
2022-10-31 17:05:48 +0900

    不備の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:edit, :show]
 
   def index
@@ -23,17 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if user_signed_in?
-      if current_user.id != @item.user_id
-        redirect_to root_path
-      end
-    else
-        redirect_to new_user_session_path
+    if current_user.id != @item.user_id
+      redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
