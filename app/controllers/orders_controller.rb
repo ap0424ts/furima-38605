e902b2ae1a92163da251c5d9770a5742ec8a65d3
ap0424ts <ap0424ts@icloud.com>
2022-11-01 14:39:46 +0900

    購入フォームの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
   def index
     @order_shipping = OrderShipping.new
   end
+
+  def create
+    binding.pry
+  end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,16 +1,17 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_code, :state, :city, :addres, :buildig, :phone_number
+  attr_accessor :item_id, :user_id, :post_code, :area_id, :city, :addres, :buildig, :phone_number
 
   with_options presence: true do
     validates :item_id
     validates :user_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :state
     validates :city
     validates :addres
     validates :phone_number, format: {with: /\A\d{10}$|^\d{11}\z/}
   end
+
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
