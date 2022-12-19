d43282d479aaf0e78404537a9c7554ef96c0fc08
ap0424ts <ap0424ts@icloud.com>
2022-12-19 21:36:42 +0900

    ヘッダーのjQuery追加
    (マウスホバー時のカラー変更)

--- app/javascript/jquery.js
+++ app/javascript/jquery.js
@@ -1,2 +1,47 @@
 $(function(){
+  $('.login').hover(
+    function(){
+      $(this).css('color','gray');
+    },
+    function(){
+      $(this).css('color','black');
+    }
+    );
+
+    $('.user-nickname').hover(
+      function(){
+        $(this).css('color','gray');
+      },
+      function(){
+        $(this).css('color','black');
+      }
+      );
+
+      $('.logout').hover(
+        function(){
+          $(this).css('color','gray');
+        },
+        function(){
+          $(this).css('color','black');
+        }
+        );
+
+        $('.brand-list').hover(
+          function(){
+            $(this).css('color','gray');
+          },
+          function(){
+            $(this).css('color','black');
+          }
+          );
+
+          $('.category-list').hover(
+            function(){
+              $(this).css('color','gray');
+            },
+            function(){
+              $(this).css('color','black');
+            }
+            );
+        
 });
